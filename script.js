ccff579da798b54dab821157c3c75f963ab046c7
HM Street Vibes <mheduardo2204@gmail.com>
2025-07-06 16:40:28 -0600

    V1.2.9

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
-function verDetalles() {
-  window.location.href = "detalles.html";
+function mostrarAlerta() {
+  const modal = document.getElementById('modalAlerta');
+  modal.style.display = 'flex'; // Mostrar modal
+
+  const btn = document.getElementById('btnEntendido');
+  btn.onclick = () => {
+    modal.style.display = 'none'; // Ocultar modal
+    window.location.href = "detalles.html"; // Redirigir
+  };
 }
